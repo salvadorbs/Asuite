183c14525242eea25060cac7d146219afff0fbed
Matteo Salvi <salvadorbs@gmail.com>
2020-04-13 11:54:16 +0200

    Formatted and restore class helper Virtualtree hack (see https://stackoverflow.com/questions/37351215/how-to-access-a-private-field-from-a-class-helper-in-delphi-10-1-berlin)

--- Library/VirtualTree.Helper.pas
+++ Library/VirtualTree.Helper.pas
@@ -1,20 +1,20 @@
 {
-Copyright (C) 2006-2015 Matteo Salvi
+  Copyright (C) 2006-2015 Matteo Salvi
 
-Website: http://www.salvadorsoftware.com/
+  Website: http://www.salvadorsoftware.com/
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 }
 
 unit VirtualTree.Helper;
@@ -40,21 +40,28 @@ var
 const
   MouseButtonDown = [tsLeftButtonDown, tsMiddleButtonDown, tsRightButtonDown];
 begin
- if Self.FCurrentHotNode <> Value then
- begin
-   DoInvalidate := (toHotTrack in THackOptions(Self.FOptions).PaintOptions) or (toCheckSupport in THackOptions(Self.FOptions).MiscOptions);
-   DoHotChange(Self.FCurrentHotNode, Value);
-   //Invalidate old Self.FCurrentHotNode
-   if Assigned(Self.FCurrentHotNode) and DoInvalidate then
-     InvalidateNode(Self.FCurrentHotNode);
-   //Set new Self.FCurrentHotNode and invalidate it
-   Self.FCurrentHotNode := Value;
-   if Assigned(Self.FCurrentHotNode) and DoInvalidate then
-     InvalidateNode(Self.FCurrentHotNode);
-   //Scroll view
-   if (Self.FUpdateCount = 0) and not (toDisableAutoscrollOnFocus in THackOptions(Self.FOptions).AutoOptions) then
-     ScrollIntoView(Self.FCurrentHotNode, (toCenterScrollIntoView in THackOptions(Self.FOptions).SelectionOptions) and
-        (MouseButtonDown * Self.FStates = []), not (toFullRowSelect in THackOptions(Self.FOptions).SelectionOptions) );
+  with Self do begin
+    if FCurrentHotNode <> Value then
+    begin
+      DoInvalidate := (toHotTrack in THackOptions(FOptions).PaintOptions) or
+        (toCheckSupport in THackOptions(FOptions).MiscOptions);
+      DoHotChange(FCurrentHotNode, Value);
+      // Invalidate old FCurrentHotNode
+      if Assigned(FCurrentHotNode) and DoInvalidate then
+        InvalidateNode(FCurrentHotNode);
+      // Set new FCurrentHotNode and invalidate it
+      FCurrentHotNode := Value;
+      if Assigned(FCurrentHotNode) and DoInvalidate then
+        InvalidateNode(FCurrentHotNode);
+      // Scroll view
+      if (FUpdateCount = 0) and
+        not(toDisableAutoscrollOnFocus in THackOptions(FOptions).AutoOptions)
+      then
+        ScrollIntoView(FCurrentHotNode,
+          (toCenterScrollIntoView in THackOptions(FOptions).SelectionOptions)
+          and (MouseButtonDown * FStates = []),
+          not(toFullRowSelect in THackOptions(FOptions).SelectionOptions));
+    end;
   end;
 end;
 
