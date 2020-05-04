cc91c30b8fc804a302e404ed986560332214428d
Matteo Salvi <salvadorbs@gmail.com>
2020-05-04 17:35:48 +0200

    Moved batch files in utils folder

--- utils/build_debug64_asuite.bat
+++ utils/build_debug64_asuite.bat
@@ -1,3 +1,3 @@
-call clean.bat
-call .\utils\Run-Dependend-rsvars-From-Path.bat L rsvars
+call ..\clean.bat
+call .\Run-Dependend-rsvars-From-Path.bat L rsvars
 msbuild /nologo /t:rebuild /verbosity:quiet /p:Platform=Win64 /p:config=Debug ASuite.dproj
