a59fbc6e46fc13b156115fa26bed237e8d2bbdf8
Matteo Salvi <salvadorbs@gmail.com>
2020-08-29 18:10:24 +0200

    Removed useless files

--- utils/build_debug64_asuite.bat
+++
@@ -1,6 +0,0 @@
-cd ..
-call clean.bat
-call .\utils\Run-Dependend-rsvars-From-Path.bat L rsvars
-msbuild /nologo /t:rebuild /verbosity:quiet /p:Platform=Win64 /p:config=Debug ASuite.dproj
-
-cd utils
--- utils/build_release_asuite.bat
+++
@@ -1,6 +0,0 @@
-cd ..
-call clean.bat
-call .\utils\Run-Dependend-rsvars-From-Path.bat L rsvars
-msbuild /nologo /t:rebuild /verbosity:quiet /p:Platform=Win32 /p:config=Release ASuite.dproj
-
-cd utils
