363c9243762874db798644a97abedf614314c511
Matteo Salvi <salvadorbs@gmail.com>
2026-09-25 23:21:38 +0200

    fix(run): make run action enabling independent of selection order

--- Utilities/Utility.RunController.pas
+++ Utilities/Utility.RunController.pas
@@ -30,32 +30,48 @@ var
   Nodes: TNodeArray;
   NodeData: TvBaseNodeData;
   I: Integer;
+  RunMode: TRunMode;
 begin
   if not Assigned(Action) or not Assigned(Tree) then Exit;
   Nodes := Tree.GetSortedSelection(True);
   Action.Enabled := False;
+  RunMode := TRunMode(Action.Tag);
 
+  // The action is enabled when at least one selected node can handle it.
+  // The result does not depend on the order of the selection.
   for I := Low(Nodes) to High(Nodes) do
   begin
     NodeData := TVirtualTreeMethods.GetNodeItemData(Nodes[I], Tree);
     if not Assigned(NodeData) then
       Continue;
 
-    if not (NodeData.IsSeparatorItem) then
-      Action.Enabled := True;
+    case RunMode of
+      rmNormal:
+        // Run: any node but a separator (categories run their children).
+        if not NodeData.IsSeparatorItem then
+        begin
+          Action.Enabled := True;
+          Break;
+        end;
 
-    if ((Action.Tag = 1) or (Action.Tag = 2)) and (NodeData.IsFileItem) then
-      Action.Enabled := IsExecutableFile(TvFileNodeData(NodeData).PathAbsoluteFile);
+      rmAsUser, rmAsAdmin:
+        // Run as/as admin: categories are allowed, files must be executable.
+        if (not NodeData.IsSeparatorItem) and
+           ((not NodeData.IsFileItem) or
+            IsExecutableFile(TvFileNodeData(NodeData).PathAbsoluteFile)) then
+        begin
+          Action.Enabled := True;
+          Break;
+        end;
 
-    if (Action.Tag = 3) then
-    begin
-      if NodeData.IsFileItem then
-      begin
-        if IsValidURLProtocol(TvFileNodeData(NodeData).PathAbsoluteFile) then
-          Action.Enabled := False
-      end
-      else
-        Action.Enabled := False;
+      rmExplorePath:
+        // Open file location: only real file paths, not URL protocols.
+        if NodeData.IsFileItem and
+           (not IsValidURLProtocol(TvFileNodeData(NodeData).PathAbsoluteFile)) then
+        begin
+          Action.Enabled := True;
+          Break;
+        end;
     end;
   end;
 end;
